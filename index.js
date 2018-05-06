21f59de82fdc7d335e8001135ace97150c28400b
sjones88 <steven.g.jones88@gmail.com>
2018-05-06 16:26:28 -0500

    add card and faces for card-flip animation

---
+++ index.js
@@ -0,0 +1,7 @@
+$(function () {
+	let card = $('div').addClass('card');
+	let frontFace = $('div').addClass('card__face--front');
+	let backFace = $('div').addClass('card__face--back');
+	card.append(frontFace);
+	card.append(backFace);
+});
